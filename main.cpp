97412cf8c18f6b0e07e0f58108ac3e6ce2faf8dd
LebedevaAlena <loik-alena@mail.ru>
2016-11-30 02:38:08 +0200

    relized constructor

--- main.cpp
+++ main.cpp
@@ -1,9 +1,14 @@
 #include <iostream>
-
+#include <string>
+#include "people.h"
 using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    people ob("Alena", 18, "Woman");
+
+    cout << "name  " << ob.GetName() << endl;
+    cout << "year" << ob.getYear() << endl;
+    cout << "nature" << ob.GetNature() << endl;
     return 0;
 }
