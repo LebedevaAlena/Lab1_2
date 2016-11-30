16a78eadc22f3ffaa6215650e3c266cab4ac144c
LebedevaAlena <loik-alena@mail.ru>
2016-11-30 02:29:52 +0200

    realized class

--- people.cpp
+++ people.cpp
@@ -1,4 +1,7 @@
 #include "people.h"
+#include <string>
+#include <iostream>
+using namespace std;
 
 people::people()
 {
@@ -7,5 +10,43 @@ people::people()
 
 people::~people()
 {
-    //dtor
+    cout << " This is destructor" << endl;
+}
+
+people::people(string name, int year, string nature)
+{
+    cout << " this is constructor" << endl;
+    this->name = name;
+    this->year = year;
+    this->nature = nature;
+}
+
+people* people::SetName(string name)
+{
+    this->name = name;
+}
+
+int people::SetYear(int year)
+{
+    this->year = year;
+}
+
+string people::SetNature(string nature)
+{
+    this->nature = nature;
+}
+
+string people::GetName()
+{
+    return name;
+}
+
+int people::getYear()
+{
+    return year;
+}
+
+string people::GetNature()
+{
+    return nature;
 }
--- people.h
+++ people.h
@@ -1,16 +1,27 @@
 #ifndef PEOPLE_H
 #define PEOPLE_H
+#include <string>
 
-
+using namespace std;
 class people
 {
     public:
         people();
         virtual ~people();
-
+        people(string name, int year, string nature);
+        people* SetName(string);
+        int SetYear(int);
+        string SetNature(string);
+        string GetName();
+        int getYear();
+        string GetNature();
     protected:
 
     private:
+        string name;
+        int year;
+        string nature;
+        friend void Show(people&);
 };
 
 #endif // PEOPLE_H
